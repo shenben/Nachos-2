26f88697fa46a07807cbaf107492880632e0953d
Natalie I Popescu <npopescu@ucsd.edu>
2018-06-04 10:46:18 -0700

    swap tests work for part 1, matmult does not yet

--- nachos/vm/VMKernel.java
+++ nachos/vm/VMKernel.java
@@ -4,6 +4,7 @@ import nachos.machine.*;
 import nachos.threads.*;
 import nachos.userprog.*;
 import nachos.vm.*;
+import java.util.*;
 
 /**
  * A kernel that can support multiple demand-paging user processes.
@@ -21,6 +22,14 @@ public class VMKernel extends UserKernel {
 	 */
 	public void initialize(String[] args) {
 		super.initialize(args);
+    numPhysPages = Machine.processor().getNumPhysPages();
+    System.out.println("numPhysPages: " + numPhysPages);
+    invTable = new TranslationEntry[numPhysPages];
+    for (int i = 0; i < numPhysPages; i++) {
+      invTable[i] = new TranslationEntry(-1, -1, true, false, false, false);
+    }
+    swapFile = ThreadedKernel.fileSystem.open("swapFile", true);
+    pagesInMem = new LinkedList<Integer>();
 	}
 
 	/**
@@ -42,10 +51,52 @@ public class VMKernel extends UserKernel {
 	 */
 	public void terminate() {
 		super.terminate();
+    swapFile.close();
+    ThreadedKernel.fileSystem.remove("swapFile");
 	}
+
+  /**
+   * Keep track of pages loaded into memory.
+   */
+  public void loadPage() {
+  }
+
+  /**
+   * Clock Alg.
+   */
+  public int evictPage() {
+    if (UserKernel.getNumFreePages() > 0) {
+      System.out.println("Getting page from freePages");
+      return UserKernel.giveOnePage();
+    }
+
+    if (pagesInMem.size() != numPhysPages) {
+      System.out.println("Memory should be filled");
+      return -1;
+    }
+
+    System.out.println("Getting page from evicted page");
+    
+    int lp = lastPos;
+    do {
+      Integer page = pagesInMem.get(lastPos);
+      lastPos = (++lastPos) % numPhysPages;
+      return page;
+    } while (lastPos != lp);
+  }
 
 	// dummy variables to make javac smarter
 	private static VMProcess dummy1 = null;
 
 	private static final char dbgVM = 'v';
+
+  public static TranslationEntry[] invTable;
+
+  public static OpenFile swapFile;
+
+  public static int numPhysPages;
+
+  public static LinkedList<Integer> pagesInMem;
+
+  private int lastPos = 0;
 }
--- nachos/vm/VMProcess.java
+++ nachos/vm/VMProcess.java
@@ -39,6 +39,7 @@ public class VMProcess extends UserProcess {
 	 * @return <tt>true</tt> if successful.
 	 */
 	protected boolean loadSections() {
+    System.out.println("numPages for process = " + numPages);
     pageTable = new TranslationEntry[numPages];
 		for( int i = 0 ; i < numPages ; i++ ) {
       pageTable[i] = new TranslationEntry(i, -1, false, false, false, false);
@@ -54,8 +55,9 @@ public class VMProcess extends UserProcess {
 	}
 
 	public int readVirtualMemory(int vaddr, byte[] data, int offset, int length) {
-    int vPage = vaddr / pageSize;
-    if (!pageTable[vPage].valid) {
+    System.out.println("in ReadVM, vaddr = " + vaddr);
+    int vpn = vaddr / pageSize;
+    if (!pageTable[vpn].valid) {
       return handlePageFault(vaddr);
     } else {
       return super.readVirtualMemory(vaddr, data, offset, length);
@@ -63,8 +65,9 @@ public class VMProcess extends UserProcess {
   }
 
 	public int writeVirtualMemory(int vaddr, byte[] data, int offset, int length) {
-    int vPage = vaddr / pageSize;
-    if (!pageTable[vPage].valid) {
+    System.out.println("in WriteVM, vaddr = " + vaddr);
+    int vpn = vaddr / pageSize;
+    if (!pageTable[vpn].valid) {
       return handlePageFault(vaddr);
     } else {
       return super.writeVirtualMemory(vaddr, data, offset, length);
@@ -75,26 +78,105 @@ public class VMProcess extends UserProcess {
    * Handler for page faults. 
    */
   private int handlePageFault(int faddr) {
-    // code, data, or stack page?
+    if (faddr < 0 || faddr >= (numPages * pageSize)) return -1;
+
+    System.out.println("faddr: " + faddr);
+
     int numCoff = coff.getNumSections();
-    int sec = faddr / pageSize;
-    if (sec < numCoff) {
-      // Load from coff or swap if not RO TODO how to deal w swap files?
-      // use dirty bits to check if non-RO gotten from swap?
-      CoffSection section = coff.getSection(sec);
-      section.loadPage(section.getFirstVPN() + sec, pageTable[sec].ppn); //TODO why need to loop?
-      if (section.isReadOnly()) pageTable[sec].readOnly = true;
+    int vpn = faddr / pageSize;
+    int ppn = -1;
+
+    System.out.println("pageTable[vpn].valid = " + pageTable[vpn].valid);
+
+    // Get a physical page
+    if (UserKernel.getNumFreePages() > 0) {
+      // If free page in list
+      System.out.println("Getting page from freePages");
+      ppn = UserKernel.giveOnePage();
+      VMKernel.invTable[ppn].vpn = vpn;
+      VMKernel.invTable[ppn].ppn = processID;
     } else {
-      // 0-fill
-      byte[] buf = new byte[pageSize];
-      for (int i = 0; i < pageSize; i++) {
-        buf[i] = 0;
+      // If need to evict an existing page
+      /*System.out.println("Getting page from evicted page");
+      for (int i = 0; i < VMKernel.numPhysPages; i++) { //TODO replace w Clock Alg
+        //if (pinned) continue;
+        if (VMKernel.invTable[i].dirty == true) {
+          byte buf[] = new byte[pageSize];
+          // fill buf[] TODO read from page?
+          VMKernel.swapFile.write(buf, 0, pageSize);
+        }
+        ppn = i;
+        VMKernel.invTable[ppn].vpn = vpn;
+        VMKernel.invTable[ppn].ppn = processID;
+      }*/
+      System.out.println("Not enough pages!");
+    }
+    /*ppn = VMKernel.evictPage();
+    VMKernel.invTable[ppn].vpn = vpn;
+    VMKernel.invTable[ppn].ppn = processID;*/
+
+    if (ppn == -1) {
+      System.out.println("No available pages to swap out!");
+      return -1; //TODO request must be delayed - CV??
+    }
+
+    // Invalidate PTE for process w this evicted page TODO
+    int pid = VMKernel.invTable[ppn].ppn;
+
+    System.out.println("vpn = " + vpn);
+    System.out.println("ppn = " + ppn);
+    System.out.println("processID = " + processID);
+
+    // Load page into memory
+    boolean coffSec = false;
+    pageTable[vpn].ppn = ppn;
+    // Loop through COFF Sections to determine if load from here
+    for (int s = 0; s < numCoff; s++) {
+      CoffSection section = coff.getSection(s);
+      for (int i = 0; i < section.getLength(); i++) {
+        if (vpn == (section.getFirstVPN() + i)) {
+          coffSec = true;
+          // Load from SWAP if dirty
+          if (pageTable[vpn].dirty) {
+            System.out.println("Loading from SWAP (code/data)");
+            // swapFile.read();//read(byte buf[], int offset, int length)
+            break;
+          // Load from COFF if not dirty
+          } else {
+            System.out.println("Loading from COFF");
+            section.loadPage(i, pageTable[vpn].ppn);
+            if (section.isReadOnly()) pageTable[vpn].readOnly = true;
+            break;
+          }
+        }
       }
-      writeVirtualMemory(faddr, buf, 0, pageSize);
+    }
+
+    // Either stack or args page
+    if (coffSec == false) {
+      // Load from SWAP if dirty
+      if (pageTable[vpn].dirty) {
+        System.out.println("Loading from SWAP (stack/args)");
+        // swapFile.read();//read(byte buf[], int offset, int length)
+      // Load as 0-filled if not dirty
+      } else {
+        System.out.println("Loading as 0-filled");
+        /*byte[] buf = new byte[pageSize];
+        for (int i = 0; i < pageSize; i++) {
+          buf[i] = 0;
+        }
+        writeVirtualMemory(vpn * pageSize, buf, 0, pageSize);*/
+        byte[] zeroFill = new byte[pageSize];
+			  System.arraycopy( zeroFill, 0, Machine.processor().getMemory(), 
+			      ppn * pageSize, pageSize );
+      }
     }
     // Mark page as valid
-    pageTable[sec].valid = true;
-    return -1;
+    pageTable[vpn].valid = true;
+    //pageTable[vpn].used = false;
+    //pageTable[vpn].dirty = false;
+    Machine.processor().setPageTable(pageTable);
+    return 1;
   }
 
 	/**
@@ -119,6 +201,11 @@ public class VMProcess extends UserProcess {
 
 	/** This process's page table. */
 	protected TranslationEntry[] pageTable;
+
+  /** How to get VMProcess from PID. */
+  private VMProcess getProcess(int pid) { //TODO
+    return this;
+  }
 
   /** Given. */
 	private static final int pageSize = Processor.pageSize;
